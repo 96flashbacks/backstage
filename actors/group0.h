ddb9df05af58504150efe16fea04f3f96215f79a
mGardener <100494379+mTakeAgain@users.noreply.github.com>
2025-12-30 15:28:28 -0300

    temporary character switch/luigi

--- actors/group0.h
+++ actors/group0.h
@@ -23,6 +23,7 @@ extern const Gfx gfx_burn_smoke_v8[];
 
 // mario
 extern Hierarchy RCP_MarioHierarchy[];
+extern Hierarchy RCP_LuigiHierarchy[];
 
 // sparkle
 extern const GeoLayout sparkles_geo[];
